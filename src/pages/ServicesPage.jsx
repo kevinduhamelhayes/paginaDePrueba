9dad4c118129c5c50ff8d7fb41cac0d3d954f296
kevinduhamelhayes <kevinduhamelh@gmail.com>
2023-07-20 05:33:59 -0300

    agregando grids

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -17,29 +17,160 @@ export default function ServicesPage() {
           <img src="./public/card-left.svg" alt="" />
         </figure>
         <div className="card-container-left">
-          <h2>Product Launch</h2>
-          <p>
+          <h2 className="tittle-card-left">Product Launch</h2>
+          <p className="p-card-left">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi
             reprehenderit quae ab natus aperiam hic itaque eos fugit accusamus,
-            debitis provident explicabo blanditiis. Magni?
+            debitis provident explicabo blanditiis. Magni? Lorem ipsum dolor
+            sit, amet consectetur adipisicing elit. Similique, beatae, id
+            ducimus incidunt iure est inventore ad sed dignissimos doloribus
+            magnam officiis esse eius nesciunt molestias quasi fugiat illo
+            rerum, quisquam in.
           </p>
-          <button> read more </button>
+          <button className="btn-card-left"> read more </button>
         </div>
-        <div className="cards-rigth">
-          <figure className="figure-right">
-            <img src="./public/card-rigth.svg" alt="" />
-          </figure>
-          <div className="card-container-rigth">
-            <h2>Product Launch</h2>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi
-              reprehenderit quae ab natus aperiam hic itaque eos fugit
-              accusamus, debitis provident explicabo blanditiis. Magni?
+      </div>
+      <div className="cards-right">
+        <div className="card-container-right">
+          <h2 className="tittle-card-right">Product Launch</h2>
+          <p className="p-card-right">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi
+            reprehenderit quae ab natus aperiam hic itaque eos fugit accusamus,
+            debitis provident explicabo blanditiis. Magni? Lorem ipsum dolor
+            sit, amet consectetur adipisicing elit. Similique, beatae, id
+            ducimus incidunt iure est inventore ad sed dignissimos doloribus
+            magnam officiis esse eius nesciunt molestias quasi fugiat illo
+            rerum, quisquam in.Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Modi reprehenderit quae ab natus aperiam hic
+            itaque eos fugit accusamus, debitis provident explicabo blanditiis.
+            Magni?
+          </p>
+          <button className="btn-card-right"> read more </button>
+        </div>
+        <figure className="figure-right">
+          <img src="./public/card-rigth.svg" alt="" />
+        </figure>
+      </div>{" "}
+      <div className="cards-left">
+        <figure className="figure-left">
+          <img src="./public/card-left.svg" alt="" />
+        </figure>
+        <div className="card-container-left">
+          <h2 className="tittle-card-left">Product Launch</h2>
+          <p className="p-card-left">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi
+            reprehenderit quae ab natus aperiam hic itaque eos fugit accusamus,
+            debitis provident explicabo blanditiis. Magni? Lorem ipsum dolor
+            sit, amet consectetur adipisicing elit. Similique, beatae, id
+            ducimus incidunt iure est inventore ad sed dignissimos doloribus
+            magnam officiis esse eius nesciunt molestias quasi fugiat illo
+            rerum, quisquam in.
+          </p>
+          <button className="btn-card-left"> read more </button>
+        </div>
+      </div>
+      <div className="cards-right">
+        <div className="card-container-right">
+          <h2 className="tittle-card-right">Product Launch</h2>
+          <p className="p-card-right">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi
+            reprehenderit quae ab natus aperiam hic itaque eos fugit accusamus,
+            debitis provident explicabo blanditiis. Magni? Lorem ipsum dolor
+            sit, amet consectetur adipisicing elit. Similique, beatae, id
+            ducimus incidunt iure est inventore ad sed dignissimos doloribus
+            magnam officiis esse eius nesciunt molestias quasi fugiat illo
+            rerum, quisquam in.Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Modi reprehenderit quae ab natus aperiam hic
+            itaque eos fugit accusamus, debitis provident explicabo blanditiis.
+            Magni?
+          </p>
+          <button className="btn-card-right"> read more </button>
+        </div>
+        <figure className="figure-right">
+          <img src="./public/card-rigth.svg" alt="" />
+        </figure>
+      </div>{" "}
+      <div className="cards-left">
+        <figure className="figure-left">
+          <img src="./public/card-left.svg" alt="" />
+        </figure>
+        <div className="card-container-left">
+          <h2 className="tittle-card-left">Product Launch</h2>
+          <p className="p-card-left">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi
+            reprehenderit quae ab natus aperiam hic itaque eos fugit accusamus,
+            debitis provident explicabo blanditiis. Magni? Lorem ipsum dolor
+            sit, amet consectetur adipisicing elit. Similique, beatae, id
+            ducimus incidunt iure est inventore ad sed dignissimos doloribus
+            magnam officiis esse eius nesciunt molestias quasi fugiat illo
+            rerum, quisquam in.
+          </p>
+          <button className="btn-card-left"> read more </button>
+        </div>
+      </div>
+      <div className="cards-right">
+        <div className="card-container-right">
+          <h2 className="tittle-card-right">Product Launch</h2>
+          <p className="p-card-right">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi
+            reprehenderit quae ab natus aperiam hic itaque eos fugit accusamus,
+            debitis provident explicabo blanditiis. Magni? Lorem ipsum dolor
+            sit, amet consectetur adipisicing elit. Similique, beatae, id
+            ducimus incidunt iure est inventore ad sed dignissimos doloribus
+            magnam officiis esse eius nesciunt molestias quasi fugiat illo
+            rerum, quisquam in.Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Modi reprehenderit quae ab natus aperiam hic
+            itaque eos fugit accusamus, debitis provident explicabo blanditiis.
+            Magni?
+          </p>
+          <button className="btn-card-right"> read more </button>
+        </div>
+        <figure className="figure-right">
+          <img src="./public/card-rigth.svg" alt="" />
+        </figure>
+      </div>{" "}
+      <div className="tittle-grid-card">
+        <h1>we Tread you like a family</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et culpa eos
+          quibusdam at, ipsa velit!
+        </p>
+        <div className="cards-grid">
+          <div className="column1">
+            <h1 className="h1-grid">you money is safe</h1>
+            <p className="text-grid-card">
+              inventore? Ullam odit eveniet numquam nesciunt? Exercitationem quisquam ex reiciendis veniam impedit sapiente? Distinctio neque voluptatibus vitae. Soluta quibusdam iste vitae adipisci nam pariatur porro enim reprehenderit? Expedita deleniti beatae facere! Quo, consectetur atque! Quae eos hic nobis suscipit. Assumenda laborum iste debitis, hic soluta doloremque architecto possimus mollitia obcaecati autem, quis similique quisquam i sit amet consectetur adipisicing elit. Dolor
+              voluptatum optio nobis ut quo quaerat!
             </p>
-            <button> read more </button>
+            <p className="text-grid-card">
+               inventore? Ullam odit eveniet numquam nesciunt? Exercitationem quisquam ex reiciendis veniam impedit sapiente? Distinctio neque voluptatibus vitae. Soluta quibusdam iste vitae adipisci nam pariatur porro enim reprehenderit? Expedita deleniti beatae facere! Quo, consectetur atque! Quae eos hic nobis suscipit. Assumenda laborum iste debitis, hic soluta doloremque architecto possimus mollitia obcaecati autem, quis similique quisquam i sit amet consectetur adipisicing elit. Dolor
+              voluptatum optio nobis ut quo quaerat!
+            </p>
+          </div>
+          <div className="column2">
+            <h1 className="h1-grid">you money is safe</h1>
+            <p className="text-grid-card">
+               inventore? Ullam odit eveniet numquam nesciunt? Exercitationem quisquam ex reiciendis veniam impedit sapiente? Distinctio neque voluptatibus vitae. Soluta quibusdam iste vitae adipisci nam pariatur porro enim reprehenderit? Expedita deleniti beatae facere! Quo, consectetur atque! Quae eos hic nobis suscipit. Assumenda laborum iste debitis, hic soluta doloremque architecto possimus mollitia obcaecati autem, quis similique quisquam i sit amet consectetur adipisicing elit. Dolor
+              voluptatum optio nobis ut quo quaerat!
+            </p>
+            <p className="text-grid-card">
+               inventore? Ullam odit eveniet numquam nesciunt? Exercitationem quisquam ex reiciendis veniam impedit sapiente? Distinctio neque voluptatibus vitae. Soluta quibusdam iste vitae adipisci nam pariatur porro enim reprehenderit? Expedita deleniti beatae facere! Quo, consectetur atque! Quae eos hic nobis suscipit. Assumenda laborum iste debitis, hic soluta doloremque architecto possimus mollitia obcaecati autem, quis similique quisquam i sit amet consectetur adipisicing elit. Dolor
+              voluptatum optio nobis ut quo quaerat!
+            </p>
+          </div>
+          <div className="column3">
+            <h1 className="h1-grid">you money is safe</h1>
+            <p className="text-grid-card">
+               inventore? Ullam odit eveniet numquam nesciunt? Exercitationem quisquam ex reiciendis veniam impedit sapiente? Distinctio neque voluptatibus vitae. Soluta quibusdam iste vitae adipisci nam pariatur porro enim reprehenderit? Expedita deleniti beatae facere! Quo, consectetur atque! Quae eos hic nobis suscipit. Assumenda laborum iste debitis, hic soluta doloremque architecto possimus mollitia obcaecati autem, quis similique quisquam i sit amet consectetur adipisicing elit. Dolor
+              voluptatum optio nobis ut quo quaerat!
+            </p>
+            <p className="text-grid-card">
+               inventore? Ullam odit eveniet numquam nesciunt? Exercitationem quisquam ex reiciendis veniam impedit sapiente? Distinctio neque voluptatibus vitae. Soluta quibusdam iste vitae adipisci nam pariatur porro enim reprehenderit? Expedita deleniti beatae facere! Quo, consectetur atque! Quae eos hic nobis suscipit. Assumenda laborum iste debitis, hic soluta doloremque architecto possimus mollitia obcaecati autem, quis similique quisquam i sit amet consectetur adipisicing elit. Dolor
+              voluptatum optio nobis ut quo quaerat!
+            </p>
           </div>
         </div>
-      </div>{" "}
+      </div>
     </div>
   )
 }
